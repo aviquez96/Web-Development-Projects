b1c6541eb60ce3933e126e364c44283ce955e504
Adrian Viquez <adrianviquez@Adrians-MacBook-Pro-2.local>
2018-09-23 23:49:08 -0700

    More express examples

---
+++ FirstExpressApp/PostRequestDemo/app.js
@@ -0,0 +1,19 @@
+var express = require ('express');
+var app = express();
+var port = 3000;
+
+app.get('/', function(req,res){
+    res.render("home.ejs");
+})
+
+// friends
+app.get('/friends', function(req,res){
+    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
+    res.render("friends.ejs", {friends: friends});
+})
+
+app.post('/addfriend', function(req, res){
+    res.send("You have reached the post route");
+})
+
+app.listen(port,console.log("Server is running!"));
