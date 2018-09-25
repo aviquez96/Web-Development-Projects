ee834c83d5caaed09f98414b200e64a41ab76cfa
Adrian Viquez <adrianviquez@Adrians-MacBook-Pro-2.local>
2018-09-24 22:59:57 -0700

    Understanding of Post Requests

--- FirstExpressApp/PostRequestDemo/app.js
+++ FirstExpressApp/PostRequestDemo/app.js
@@ -1,6 +1,12 @@
 var express = require ('express');
+var bodyParser = require ('body-parser');
 var app = express();
 var port = 3000;
+
+// Allows to interpret the body into an object
+app.use(bodyParser.urlencoded({extended: true}));
+
+var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
 
 app.get('/', function(req,res){
     res.render("home.ejs");
@@ -8,12 +14,15 @@ app.get('/', function(req,res){
 
 // friends
 app.get('/friends', function(req,res){
-    var friends = ["Tony", "Miranda", "Justin", "Pierre", "Lily"];
     res.render("friends.ejs", {friends: friends});
 })
 
 app.post('/addfriend', function(req, res){
-    res.send("You have reached the post routee");
+    // The newfriend variable is what we added as "name" property in the friends.ejs file
+    var newfriend = req.body.newfriend;
+    friends.push(newfriend);
+    // res.redirect goes to the specified channel, 
+    res.redirect("/friends");
 })
 
 app.listen(port,console.log("Server is running!"));
