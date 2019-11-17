0696fe14b2a1f945c69fb682ed73f29691be0837
Adrian Viquez <aviquez96@gmail.com>
2019-11-16 23:57:23 -0800

    Learn more about fetch

--- Ajax/fetch.js
+++ Ajax/fetch.js
@@ -23,3 +23,29 @@ fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
 // .then(function(data) {
 //     console.log(data);
 // })
+
+
+//More complete fetch example
+// var btn = document.querySelector("button");
+// btn.addEventListener("click", function(){
+  
+//   var url = 'https://api.github.com/users/colt';
+  
+//   fetch(url)
+//   .then(handleErrors)
+//   .then(function(request) {
+//     console.log("Everything is fine");
+//     console.log(request);
+//   }) 
+//   //Code run if there's a problem with the request itself, not the value of the response
+//   .catch(function(error) {
+//     console.log(error);
+//   })  
+// })
+
+// function handleErrors (request) {
+//   if(!request.ok) {
+//     throw Error("Erroooor");
+//   }
+//   return request;
+// }
