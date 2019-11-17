b3c3da4b7850538bbaa1979a822b25a52e93fc2a
Adrian Viquez <aviquez96@gmail.com>
2019-11-16 23:05:36 -0800

    Another example using bitcoin's price API

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -31,3 +31,26 @@ XHR.send();
 //   XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
 //   XHR.send();
 // })
+
+
+//Independent request using bitcoin API
+// let price = document.querySelector("#price");
+// let btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", function() {
+  
+//   let XHR = new XMLHttpRequest();
+  
+//   XHR.onreadystatechange = function () {
+//     if (XHR.readyState === 4 && XHR.status === 200) { 
+//       let data = JSON.parse(XHR.responseText).bpi;
+//       let amount = data.USD.rate;
+//       let code = data.USD.code;
+//       price.innerText = amount + " " + code;
+//     }
+//   }
+
+//   XHR.open("GET", "https://api.coindesk.com/v1/bpi/currentprice.json");
+//   XHR.send();
+  
+//   })
