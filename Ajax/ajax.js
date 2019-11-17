bed4aa36e78a3120f1a6cef8360be536091ad286
Adrian Viquez <aviquez96@gmail.com>
2019-11-16 23:04:49 -0800

    Making a more representative call

--- Ajax/ajax.js
+++ Ajax/ajax.js
@@ -12,3 +12,22 @@ XHR.onreadystatechange = function() {
 
 XHR.open("GET", "https://api.github.com/zen");
 XHR.send();
+
+
+// Get some meanigful data through ajax
+// let btn = document.querySelector("#btn");
+// let img = document.querySelector("#photo");
+
+// btn.addEventListener("click", function(){
+//   var XHR = new XMLHttpRequest();
+  
+//   XHR.onreadystatechange = function() {
+//     if (XHR.readyState === 4 && XHR.status === 200) {
+//       let url = JSON.parse(XHR.responseText).message;
+//       img.src = url;
+//     }
+//   }
+  
+//   XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+//   XHR.send();
+// })
