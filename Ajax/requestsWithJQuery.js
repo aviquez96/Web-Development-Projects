f2be1e02bc61936dc7bfdaf3b2eacda564f14f4f
Adrian Viquez <aviquez96@gmail.com>
2019-11-17 15:46:11 -0800

    a

--- Ajax/requestsWithJQuery.js
+++ Ajax/requestsWithJQuery.js
@@ -13,3 +13,47 @@ $("#btn").click(function() {
     })
     
   })
+  
+
+//   $("#btn").click(function() {
+  
+//     $.ajax({
+//       method: "GET",
+//       url: "https://baconipsum.com/api/?type=meat-awqwerqwend=filler",
+//       dataType: 'json'
+//     })
+//     .done(function(data) {
+//       $("p").text(data[[0]])
+//     })
+//     .fail(function() {
+//       alert("Oh no!!!");
+//     })
+    
+//   })
+  
+//   //Shorthand for ajax with get method
+//   $("#get").click(function() {
+//     $.get("https://api.github.com/users/aviquez96")
+//     .done(function(data) {
+//       console.log(data);
+//     })
+//   })
+                  
+//   $("#post").click(function() {
+//     $.post("www.catsarecoolandsoaredogs.com")
+//     .done(function(data) {
+//       console.log(data)
+//     })
+//     .fail(function(err) {
+//       console.log(err);
+//     })
+//   })
+                   
+//   $("#getJSON").click(function() {
+//     $.getJSON("https://api.github.com/users/aviquez96")
+//     .done(function(data) {
+//       console.log(data);
+//     })
+//   })
+                  
+                  
